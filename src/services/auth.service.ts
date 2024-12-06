0dddd5b5b8b872153988963c27c391a5a4fedca7
dvtdat <tiendat0417@gmail.com>
2024-12-06 14:17:27 +0700

    feat: auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { User, Response } from '@/types';
+import { axios } from '@/utils/custom-axios';
+
+import { CreateUserDto } from './user.service';
+const API_URL = 'http://localhost:5000';
+
+const login = (username: string, password: string) => {
+  return axios.post<Response<User>>(`${API_URL}/auth/login`, {
+    username,
+    password,
+  });
+};
+
+const signup = (createUserDto: CreateUserDto) => {
+  return axios.post<Response<User>>(`${API_URL}/auth/signup`, createUserDto);
+};
+
+export const authService = {
+  login,
+  signup,
+};
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,10 +3,28 @@ import { axios } from '@/utils/custom-axios';
 
 const API_URL = 'http://localhost:5000';
 
+export type CreateUserDto = {
+  name: string;
+  username: string;
+  password: string;
+  email: string;
+  role: string;
+};
+
 const getAll = () => {
   return axios.get<Response<User[]>>(`${API_URL}/user`);
 };
 
+const getById = (userId: number) => {
+  return axios.get<Response<User>>(`${API_URL}/user/${userId}`);
+};
+
+const create = (createUserDto: CreateUserDto) => {
+  return axios.post<Response<User>>(`${API_URL}/user`, createUserDto);
+};
+
 export const userService = {
   getAll,
+  getById,
+  create,
 };
